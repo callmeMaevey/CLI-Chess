8c7b1dc38d0a41cacb013beba9634a4cc1067b14
Maevey <70716966+callmeMaevey@users.noreply.github.com>
2024-01-28 12:42:15 -0500

    rough draft dynamic move alloc

--- chess.h
+++ chess.h
@@ -84,6 +84,32 @@ void drawBoard( Board* state){
   }
 }
 
+
+//////////////////// untested /////////////////// maybe should be in its own header? will need ifndef
+
+int** addMoveToArray ( int** movesArray, int* move) {
+  // dymaically reallocate movesArray
+  if ( movesArray == null ) {
+    movesArray = (int**)malloc(sizeof(int*)*2);
+    movesArray[0] = move;
+    movesArray[1] = '\0';
+  }
+  int index = 0;
+  while ( movesArray[index] != '\0' ) { index++; }
+  int** newMovesArray = (int**) malloc( sizeof(int*) * index );
+  index = 0;
+  while ( movesArray[index] != '\0' ) { //derefrence might need to go to the underlying pointer
+    *newMovesArray[index] = *movesArray[index]; // copy pointers location
+    free( *( movesArray[index] ) ); //free pointer underneath
+    free( movesArray[index] ); // free pointer in array
+    index++ ;
+  }
+  free(movesArray); // free array
+
+  return newMovesArray; //return newly allocated Array
+
+}
+
 int** getValidMoves ( Board* state, int i, int j ) {
   PieceType pieceType = state -> tiles[i][j] -> type;
   Color pieceColor = state -> tiles[i][j] -> type;
